dd404d7d1893729799705fb6c764e87200217c98
FelipeMovio <felipelsmovio@gmail.com>
2026-02-24 18:59:15 -0300

    ...

--- src/main/java/com/clinicaMed/api/controler/MedicoController.java
+++ src/main/java/com/clinicaMed/api/controler/MedicoController.java
@@ -1,5 +1,6 @@
 package com.clinicaMed.api.controler;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @PostMapping
     public void cadastrar(){}
 }
