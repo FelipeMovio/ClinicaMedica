c3483d981ef0542a3eea542da77fe58f4e684c85
FelipeMovio <felipelsmovio@gmail.com>
2026-02-25 19:39:21 -0300

    PacienteController create

---
+++ src/main/java/com/clinicaMed/api/controler/PacienteController.java
@@ -0,0 +1,12 @@
+package com.clinicaMed.api.controler;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paciente")
+public class PacienteController {
+
+    public void Cadastrar(@RequestBody DadosCadastroPaciente dados){}
+}
