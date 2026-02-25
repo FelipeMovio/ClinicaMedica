9f98213f11ab49e31fb4221fab75be4e664b009e
FelipeMovio <felipelsmovio@gmail.com>
2026-02-25 19:28:51 -0300

    ...

--- src/main/java/com/clinicaMed/api/medico/DadosCadastroMedico.java
+++ src/main/java/com/clinicaMed/api/medico/DadosCadastroMedico.java
@@ -1,4 +1,6 @@
 package com.clinicaMed.api.medico;
 
+import com.clinicaMed.api.endereco.DadosEndereco;
+
 public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 }
