f79231ae1d643b814c11a68b6b78edae6d98e102
FelipeMovio <felipelsmovio@gmail.com>
2026-02-25 20:19:00 -0300

    ...

--- src/main/java/com/clinicaMed/api/controler/PacienteController.java
+++ src/main/java/com/clinicaMed/api/controler/PacienteController.java
@@ -1,5 +1,6 @@
 package com.clinicaMed.api.controler;
 
+import com.clinicaMed.api.entity.paciente.DadosCadastroPaciente;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
