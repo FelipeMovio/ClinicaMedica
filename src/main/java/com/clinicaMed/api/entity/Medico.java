fc107e9dcdd2c5d2f968ed4d66da89409ad2bca0
FelipeMovio <felipelsmovio@gmail.com>
2026-02-26 19:35:25 -0300

    ...

--- src/main/java/com/clinicaMed/api/entity/Medico.java
+++ src/main/java/com/clinicaMed/api/entity/Medico.java
@@ -1,12 +1,20 @@
 package com.clinicaMed.api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
 public class Medico {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
