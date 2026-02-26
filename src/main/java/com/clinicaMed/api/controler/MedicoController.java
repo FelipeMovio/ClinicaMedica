d07cfd13ae7476f725a569bc9b6bfe7aa86f1829
FelipeMovio <felipelsmovio@gmail.com>
2026-02-26 20:15:04 -0300

    convertendo os dtos no controller

--- src/main/java/com/clinicaMed/api/controler/MedicoController.java
+++ src/main/java/com/clinicaMed/api/controler/MedicoController.java
@@ -1,6 +1,11 @@
 package com.clinicaMed.api.controler;
 
 import com.clinicaMed.api.Dto.DadosCadastroMedicoDTO;
+import com.clinicaMed.api.Dto.DadosEnderecoDTO;
+import com.clinicaMed.api.entity.Endereco;
+import com.clinicaMed.api.entity.Medico;
+import com.clinicaMed.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedicoDTO dados){}
+    public void cadastrar(@RequestBody DadosCadastroMedicoDTO dados){
+
+
+        Medico medico = new Medico(null,
+                dados.nome(),
+                dados.email(),
+                dados.crm(),
+                dados.especialidade(),
+                new Endereco(
+                        dados.endereco().logradouro(),
+                        dados.endereco().cep(),
+                        dados.endereco().cidade(),
+                        dados.endereco().numero(),
+                        dados.endereco().complemento(),
+                        dados.endereco().uf()
+                )
+        );
+
+        repository.save(medico);
+    }
 }
