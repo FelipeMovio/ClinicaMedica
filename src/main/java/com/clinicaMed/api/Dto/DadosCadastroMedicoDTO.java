f0213958816e0da1ba2f31ac7fa1889e7564271d
FelipeMovio <felipelsmovio@gmail.com>
2026-02-27 20:05:26 -0300

    fazendo as validaçoes

--- src/main/java/com/clinicaMed/api/Dto/DadosCadastroMedicoDTO.java
+++ src/main/java/com/clinicaMed/api/Dto/DadosCadastroMedicoDTO.java
@@ -1,6 +1,29 @@
 package com.clinicaMed.api.Dto;
 
 import com.clinicaMed.api.entity.Especialidade;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record DadosCadastroMedicoDTO(String nome, String email, String crm, Especialidade especialidade, DadosEnderecoDTO endereco) {
-}
+public record DadosCadastroMedicoDTO(
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d`{4,6}")
+        String crm,
+
+        @NotNull
+        Especialidade especialidade,
+
+        @NotNull
+        @Valid
+        DadosEnderecoDTO endereco
+)
+{}
