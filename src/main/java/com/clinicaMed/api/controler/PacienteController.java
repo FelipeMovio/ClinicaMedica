12206fcfa5b5177b9073e238fe166982b4974cf4
FelipeMovio <felipelsmovio@gmail.com>
2026-02-28 12:43:28 -0300

    comecando o controller de pacientes

--- src/main/java/com/clinicaMed/api/controler/PacienteController.java
+++ src/main/java/com/clinicaMed/api/controler/PacienteController.java
@@ -1,13 +1,18 @@
 package com.clinicaMed.api.controler;
 
 import com.clinicaMed.api.Dto.DadosCadastroPacienteDTO;
+import com.clinicaMed.api.repository.PacientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/paciente")
+@RequestMapping("/pacientes")
 public class PacienteController {
+
+    @Autowired
+    private PacientesRepository repository;
 
     public void Cadastrar(@RequestBody DadosCadastroPacienteDTO dados){
     }
