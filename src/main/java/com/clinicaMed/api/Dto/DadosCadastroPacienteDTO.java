e4e673d3394430702fbf960ad41560a27458edf1
FelipeMovio <felipelsmovio@gmail.com>
2026-02-28 12:25:34 -0300

    update

--- src/main/java/com/clinicaMed/api/Dto/DadosCadastroPacienteDTO.java
+++ src/main/java/com/clinicaMed/api/Dto/DadosCadastroPacienteDTO.java
@@ -1,8 +1,13 @@
 package com.clinicaMed.api.Dto;
 
-public record DadosCadastroPacienteDTO(String nome,
-                                       String email,
-                                       String telefone,
-                                       String cpf,
-                                       DadosEnderecoDTO endereco) {
+public record DadosCadastroPacienteDTO(
+        String nome,
+
+        String email,
+
+        String telefone,
+
+        String cpf,
+
+        DadosEnderecoDTO endereco) {
 }
