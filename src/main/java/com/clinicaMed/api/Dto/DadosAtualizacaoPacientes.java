980b32ee7157515eb0dbc9a3a7404603bc04c46b
FelipeMovio <felipelsmovio@gmail.com>
2026-03-05 19:35:11 -0300

    ...

--- src/main/java/com/clinicaMed/api/Dto/DadosAtualizacaoPacientes.java
+++ src/main/java/com/clinicaMed/api/Dto/DadosAtualizacaoPacientes.java
@@ -1,12 +1,16 @@
 package com.clinicaMed.api.Dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosAtualizacaoPacientes(
         @NotNull
         Long id,
         String nome,
+
         String telefone,
+
+        @Valid
         DadosEnderecoDTO endereco
 ) {
 }
